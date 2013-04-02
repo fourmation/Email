b5f9d9c725abff05855a6d9c6a42abed9d72f114
raymond <raymond@4mation.com.au>
2013-04-02 11:30:07 +1100

    Updated config file with details necessary for sending locally

--- config/mail.config.dist.php
+++ config/mail.config.dist.php
@@ -4,5 +4,18 @@ return array(
         'from_email' => 'FILL_IN',
         'to_email' => 'FILL_IN',
         'dev_email' => 'FILL_IN',
+        'development_mode' => true, // If true, will send to dev_email above
+        'transport' => array(
+            'options' => array(
+                'host' => 'FILL_IN', // Eg smtp.gmail.com
+                'connection_class' => 'plain',
+                'connection_config' => array(
+                    'username' => 'FILL_IN',
+                    'password' => 'FILL_IN',
+                    'ssl' => 'tls'
+                ),
+            ),
+        ),
+        'site_name' => 'FILL_IN' // Eg. www.example.com
     )
 );
